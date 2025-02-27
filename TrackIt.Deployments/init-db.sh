9c10c4d1b4ee1577ea2c22245963e0ba3603527a
egor <jtqafj@gmail.com>
2025-02-27 18:19:08 +0300

    Правки

--- TrackIt.Deployments/init-db.sh
+++ TrackIt.Deployments/init-db.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
+echo "CREATING DATABASE trackit"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE trackit;
+EOSQL
+
+echo "CREATING DATABASE keycloak"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE keycloak;
 EOSQL
