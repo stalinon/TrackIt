15de53f200b73aa1efac4f0a4a6ec340bdd66a5a
egor <jtqafj@gmail.com>
2025-02-28 22:40:02 +0300

    Накидал общую структуру страницы

--- TrackIt.Frontend/src/components/Dashboard.tsx
+++ TrackIt.Frontend/src/components/Dashboard.tsx
@@ -1,11 +1,30 @@
 import React from "react";
+import "../styles/Dashboard.css";
+import { Button, Layout } from "antd";
+import logo from "../black_on_white.png";
+import Sider from "antd/es/layout/Sider";
+import { Content, Header } from "antd/es/layout/layout";
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Welcome to your Dashboard!</h1>
-      <p>You are successfully logged in.</p>
-    </div>
+    <Layout className="layout">
+      <Sider className="layout__sider" width={350}>
+        <img className="layout__sider__logo" src={logo} alt="logo" />
+        <Button className="layout__sider__btn" type="text">
+          Dashboard
+        </Button>
+        <Button className="layout__sider__btn" type="text">
+          Transactions
+        </Button>
+        <Button className="layout__sider__btn" type="text">
+          Limits
+        </Button>
+        <Button className="layout__sider__btn" type="text">
+          Planned payments
+        </Button>
+      </Sider>
+      <Content></Content>
+    </Layout>
   );
 };
 
